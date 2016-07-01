22b1c3324a94444aad6b910029de5e45c0b18c39
AoEiuV020 <490674483@qq.com>
2016-07-02 03:57:26 +0800

    okhttp3简单使用
    基本的http请求，

---
+++ okhttp/src/cc/aoeiuv020/Main.java
@@ -0,0 +1,15 @@
+/* ***************************************************
+	^> File Name: Main.java
+	^> Author: AoEiuV020
+	^> Mail: 490674483@qq.com
+	^> Created Time: 2016/07/01 - 21:33:40
+*************************************************** */
+package cc.aoeiuv020;
+public class Main
+{
+	public static void main(String[] args)throws Exception
+	{
+		//HttpTest.main(args);
+		CallbackTest.main(args);
+	}
+}
